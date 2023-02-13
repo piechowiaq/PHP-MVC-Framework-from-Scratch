1d5cb30d0d6ac3fc77b3edcb85cb88062d914e12
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-02-13 21:14:02 +0100

    Building Router

---
+++ core/Router.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    public Request $request;
+    protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false){
+            return "Not found";
+
+        }
+
+        if (is_string($callback)){
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    public function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
+
+}
