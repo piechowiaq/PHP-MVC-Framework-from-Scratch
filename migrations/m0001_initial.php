2a37cc519eaf3f30bf3fdcf53503a1d7c08b4d35
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-02-20 20:59:24 +0100

    Connecting to db

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,15 @@
+<?php
+
+class m0001_initial
+{
+
+    public function up()
+    {
+        echo "Applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migration";
+    }
+}
