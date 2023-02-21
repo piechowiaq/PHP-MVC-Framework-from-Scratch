13667be8a306f1a744448f8787c94a688d72aa6f
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-02-21 20:20:44 +0100

    Creating migration

--- core/Database.php
+++ core/Database.php
@@ -15,4 +15,33 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $this->getAppliedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR.'/migrations');
+        echo '<pre>';
+        var_dump($files);
+        echo '</pre>';
+        exit;
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            migration VARCHAR (255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
 }
