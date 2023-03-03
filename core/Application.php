9891d1cd87090a9ef70ddf87908e3cf93c99a696
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-03-03 21:08:27 +0100

    Improving form rendering

--- core/Application.php
+++ core/Application.php
@@ -2,6 +2,8 @@
 
 namespace app\core;
 
+use app\core\db\Database;
+use app\core\db\DbModel;
 use Exception;
 
 class Application
@@ -15,7 +17,7 @@ class Application
     public Response $response;
     public Session $session;
     public Database $db;
-    public ?DbModel $user;
+    public ?UserModel $user;
     public View $view;
 
     public static Application $app;
@@ -75,7 +77,7 @@ class Application
         $this->controller = $controller;
     }
 
-    public function login(DbModel $user)
+    public function login(UserModel $user)
     {
         $this->user = $user;
         $primaryKey = $user->primaryKey();
